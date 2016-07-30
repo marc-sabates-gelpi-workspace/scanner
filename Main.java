48691946467e1d5e10879cbf9c51abaa732e08d6
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2016-07-30 17:48:48 +0100

    Scanner command line example

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Integer num = scan.nextInt();
+        System.out.println("Hello World! num: "+num); // Display the string.
+    }
+}
